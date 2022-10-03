3ffea431d1824ae984708d6eb53493d131e2fecd
Daniel Markiewicz <dmarkiew@gmail.com>
2022-10-03 17:25:32 +0300

    adapt bgu

---
+++ assets/obfuscate-long.js
@@ -0,0 +1,15 @@
+
+        function encaps(each){
+        var code=[108,105,46,99,97,46,117,103,98,46,104,116,97,109,64]
+        var common='' //variable to contain e-address
+        var out='-'
+        if (each!='-')
+        {
+          for (var i=1; i<code.length+1; i++)
+          {
+          common+=String.fromCharCode(code[code.length-i])
+          }
+        out='<a href="mailto:'+each+common+'">'+each+common+'</a>'
+        }
+        return out;
+        }
